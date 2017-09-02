72673cf77c0cbcb1408819d9244ad0c3636a67b8
gargi16 <gargiyadav1699@gmail.com>
2017-09-02 23:57:38 +0530

    update note

--- app/src/main/java/gargi/droiders/com/notepad/NotesDbHelper.java
+++ app/src/main/java/gargi/droiders/com/notepad/NotesDbHelper.java
@@ -5,15 +5,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.provider.ContactsContract;
-import android.util.Log;
-
-import static android.R.attr.id;
-import static android.R.string.no;
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
-import static android.icu.text.Normalizer.NO;
-import static android.os.Build.VERSION_CODES.N;
-import static android.provider.Contacts.SettingsColumns.KEY;
 
 /**
  * Created by hp on 01-09-2017.
@@ -21,35 +12,35 @@ import static android.provider.Contacts.SettingsColumns.KEY;
 
 public class NotesDbHelper extends SQLiteOpenHelper {
 
-    static final String DATABASE_NAME = "NotepadDb" ;
-    static final int DATABASE_VERSION =1 ;
+    static final String DATABASE_NAME = "NotepadDb";
+    static final int DATABASE_VERSION = 1;
 
 
-
-
-    public  NotesDbHelper (Context context ) {   super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    public NotesDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
+
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase){
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
         final String SQL_CREATE_WAITLIST_TABLE = " CREATE TABLE " +
-                NotesContractClass.NoteListEntry.TABLE_NAME   + " ( " +
+                NotesContractClass.NoteListEntry.TABLE_NAME + " ( " +
                 NotesContractClass.NoteListEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 NotesContractClass.NoteListEntry.COLUMN_NOTES_TITLE + " TEXT NOT NULL, " +
                 NotesContractClass.NoteListEntry.COLUMN_NOTES_TEXT + " TEXT NOT NULL " +
-               " ); " ;
-           sqLiteDatabase.execSQL(SQL_CREATE_WAITLIST_TABLE);
+                " ); ";
+        sqLiteDatabase.execSQL(SQL_CREATE_WAITLIST_TABLE);
 
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-          sqLiteDatabase.execSQL("DROP TABLE IF EXIST"+ NotesContractClass.NoteListEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXIST" + NotesContractClass.NoteListEntry.TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
 
 
-    long addNote(String title, String text ) {
+    long addNote(String title, String text) {
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -57,41 +48,40 @@ public class NotesDbHelper extends SQLiteOpenHelper {
         values.put(NotesContractClass.NoteListEntry.COLUMN_NOTES_TEXT, text);
 
 
-        long id = sqLiteDatabase.insert(NotesContractClass.NoteListEntry.TABLE_NAME , null ,values);
+        long id = sqLiteDatabase.insert(NotesContractClass.NoteListEntry.TABLE_NAME, null, values);
         sqLiteDatabase.close();
 
         return id;
     }
-    long updateNote(String title , String text){
+
+    long updateNote(String title, String text, long id) {
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(NotesContractClass.NoteListEntry.COLUMN_NOTES_TITLE,title);
-        values.put(NotesContractClass.NoteListEntry.COLUMN_NOTES_TEXT,text);
+        values.put(NotesContractClass.NoteListEntry.COLUMN_NOTES_TITLE, title);
+        values.put(NotesContractClass.NoteListEntry.COLUMN_NOTES_TEXT, text);
+        sqLiteDatabase.update(NotesContractClass.NoteListEntry.TABLE_NAME, values,
+                "_id=" + id, null);
 
-
-
-       return id;
+        return id;
     }
 
 
-    public void deleteNote(Note note)
-    {
+    public void deleteNote(Note note) {
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-        sqLiteDatabase.delete(NotesContractClass.NoteListEntry.TABLE_NAME, NotesContractClass.NoteListEntry._ID  + " = ?",
-                new String[] { String.valueOf(note.getmId()) });
+        sqLiteDatabase.delete(NotesContractClass.NoteListEntry.TABLE_NAME, NotesContractClass.NoteListEntry._ID + " = ?",
+                new String[]{String.valueOf(note.getmId())});
         sqLiteDatabase.close();
     }
 
-    long getNotesCount(){
+    long getNotesCount() {
         String countQuery = "SELECT * FROM " + NotesContractClass.NoteListEntry.TABLE_NAME;
-        SQLiteDatabase sqLiteDatabase =this.getReadableDatabase();
-        Cursor cursor = sqLiteDatabase.rawQuery(countQuery,null);
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery(countQuery, null);
         cursor.close();
         return cursor.getCount();
     }
 
 
-
-    }
+}
 
--- app/src/main/java/gargi/droiders/com/notepad/SaveNoteActivity.java
+++ app/src/main/java/gargi/droiders/com/notepad/SaveNoteActivity.java
@@ -1,6 +1,8 @@
 package gargi.droiders.com.notepad;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +10,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import static com.google.android.gms.common.api.Status.sq;
+import static gargi.droiders.com.notepad.NotesContractClass.NoteListEntry.COLUMN_USER_NAME;
 
 public class SaveNoteActivity extends AppCompatActivity {
     private EditText title_editText, content_editText;
@@ -25,18 +30,49 @@ public class SaveNoteActivity extends AppCompatActivity {
         content_editText = (EditText) findViewById(R.id.et_notes_content);
         buttonSave = (Button) findViewById(R.id.save_btn);
         mDbhelper = new NotesDbHelper(this);
-       //  Bundle bundle = getIntent().getExtras();
+
 
           Intent intent = getIntent() ;
-        long id = intent.getLongExtra("identity",-1);
+        final long id = intent.getLongExtra("identity",-1);
         Toast.makeText(this , "id =" + id , Toast.LENGTH_SHORT).show();
+
+        if(id!= -1){
+            SQLiteDatabase database = mDbhelper.getReadableDatabase();
+
+            String selection = NotesContractClass.NoteListEntry._ID + " = ?";
+
+            // selection argument
+            String[] selectionArgs = {Long.toString(id)};
+
+           Cursor cursor = database.query(NotesContractClass.NoteListEntry.TABLE_NAME ,null,
+                    selection,selectionArgs,null ,null,null);
+            cursor.moveToFirst() ;
+            String title , text ;
+
+            title = cursor.getString(cursor.getColumnIndex(NotesContractClass.NoteListEntry.COLUMN_NOTES_TITLE));
+            text = cursor.getString(cursor.getColumnIndex(NotesContractClass.NoteListEntry.COLUMN_NOTES_TEXT));
+
+            title_editText.setText(title);
+            content_editText.setText(text);
+            database.close();
+        }
 
             buttonSave.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+
                     String title = title_editText.getText().toString();
                     String contentText = content_editText.getText().toString();
-                    long id = mDbhelper.addNote(title, contentText);
+
+
+                    if(id == -1)
+                    {
+                      long returnID= mDbhelper.addNote(title,contentText);
+
+                    }
+                    else
+                       mDbhelper.updateNote(title,contentText , id) ;
+
 
                     Toast.makeText(SaveNoteActivity.this, "Note id :" + id, Toast.LENGTH_SHORT).show();
 
@@ -44,6 +80,8 @@ public class SaveNoteActivity extends AppCompatActivity {
 
                     startActivity(intent);
                     finish();
+
+
 
 
                 }
