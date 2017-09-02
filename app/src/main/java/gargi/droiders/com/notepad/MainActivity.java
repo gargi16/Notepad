a6a11b180bd8334dd0f433e39725aae43ee3538c
Samagra <samagra14@gmail.com>
2017-09-02 21:47:12 +0530

    Adding ocr

--- app/src/main/java/gargi/droiders/com/notepad/MainActivity.java
+++ app/src/main/java/gargi/droiders/com/notepad/MainActivity.java
@@ -1,13 +1,25 @@
 package gargi.droiders.com.notepad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button cameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraButton = (Button) findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CameraActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
